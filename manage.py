f89bc0cf7df07b949ba54a8d2634f5d1b2d57d67
Nestedman <nestedman@gmail.com>
2020-05-26 03:29:24 +0200

    chore: add migration to launch script

--- manage.py
+++ manage.py
@@ -1,9 +1,24 @@
 """The application launch script"""
 
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from app import app
+from app.database import db
 
+
+migrate = Migrate(app, db)
 manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def reset_tables():
+    """
+    Drop and re-create all tables
+    """
+    db.drop_all()
+    db.create_all()
+
 
 if __name__ == '__main__':
     manager.run()
