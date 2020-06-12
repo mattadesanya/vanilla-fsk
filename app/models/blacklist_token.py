676ebb69562fef92a5efdbb10e48fd83d6a69f98
Nestedman <nestedman@gmail.com>
2020-06-12 23:37:46 +0200

    feat(model): add blacklist_token

---
+++ app/models/blacklist_token.py
@@ -0,0 +1,34 @@
+# pylint: disable=no-member
+# pylint: disable=too-few-public-methods
+
+"""Blacklist Model"""
+
+from datetime import datetime
+from app.database import db
+
+
+class BlacklistToken(db.Model):
+    """
+    Token Model for storing JWT tokens
+    """
+    __tablename__ = 'blacklist_tokens'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    blacklisted_on = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, token):
+        self.token = token
+        self.blacklisted_on = datetime.now()
+
+    @staticmethod
+    def check_blacklist(auth_token):
+        """ check whether auth token has been blacklisted """
+        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
+        if res:
+            return True
+
+        return False
+
+    def __repr__(self):
+        return '<id: token: {}'.format(self.token)
