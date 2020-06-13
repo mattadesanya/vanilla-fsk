2b2523581d30c3411eda1e962160810f15745be8
Nestedman <nestedman@gmail.com>
2020-06-13 13:12:41 +0200

    refactor: move create_user() to user controller

--- app/controllers/api/v1/users_controller.py
+++ app/controllers/api/v1/users_controller.py
@@ -1,12 +1,32 @@
 """Users Controller"""
 
-from flask import jsonify
+from flask import jsonify, request
+from marshmallow import ValidationError
+from app.lib.user_lib import UserLib as user_lib
 from app.controllers.api import api_v1 as api
 from app.models.user import User
 from app.serializers.api.v1.user_serializer import users_serializer, \
                                                     user_serializer
 from app.util.db_util import DbUtil as db
 from app.lib.authorization.auth_decorator import token_required
+
+
+@api.route('/users', methods=['POST'])
+def create_user():
+    """ Create a new user """
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({'message': 'no valid input'}), 400
+
+    # validate and deserialize the input
+    try:
+        new_user = user_serializer.load(json_data)
+    except ValidationError as err:
+        return jsonify(err.messages), 422
+
+    user = user_lib.create_user(new_user)
+    result = user_serializer.dump(user)
+    return jsonify({'message': 'User Created', 'data': result}), 201
 
 
 @api.route('/users')
@@ -21,14 +41,14 @@ def get_users():
     return jsonify({'data': result})
 
 
-@api.route('/users/<f_id>')
+@api.route('/users/<fancy_id>')
 @token_required
-def get_user(f_id):
+def get_user(fancy_id):
     """Calls api/v1/users/id
 
     This controller returns a particular used by id
     """
-    user = db.get_by_fid(User, f_id)
+    user = db.get_by_fid(User, fancy_id)
     result = user_serializer.dump(user)
     return jsonify({'data': result})
 
